7347bb35ab6e568763160253d5d6b4a5061e349e
Larissa Jaqueira <48590279+larissajaq@users.noreply.github.com>
2022-06-20 14:50:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  const navigation = document.querySelector('#navigation')
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// Initialize Swiper
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 4
+    }
+  }
+})
